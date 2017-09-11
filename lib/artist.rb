6266c93463f79704216eaf13355be8425c89b13b
Chris Nady <nady.christopher@gmail.com>
2017-09-11 20:23:28 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,6 +9,12 @@ class Artist
     @name = name
   end
 
+  def add_song(song)
+    @songs << song
+    @@song_count += 1
+    song.artist = self
+  end
+
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
@@ -47,12 +53,6 @@ class Artist
     artist = Artist.new(name)
     artist.save
     artist
-  end
-
-  def add_song(song)
-    @songs << song
-    @@song_count += 1
-    song.artist = self
   end
 
 end
