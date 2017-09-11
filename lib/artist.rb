a44feb1adc1669adb4745a937968b0d12cc73451
Chris Nady <nady.christopher@gmail.com>
2017-09-11 17:38:07 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -39,7 +39,7 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    if @@all.include?(name.object_id) == true
+    if @@all.include?(self.name) == true
       self
     else
       artist = Artist.new(name)
