bb3052bd0b9b9d34d473bc7c79368fc394549016
Chris Nady <nady.christopher@gmail.com>
2017-09-11 19:59:15 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -15,7 +15,10 @@ class MP3Importer
   end
 
   def import
-    files
+    @list_of_filenames = Dir["#{@path}/*.mp3"]
+    @list_of_filenames.each do |filename|
+      filename.slice!("#{@path}/")
+    end
     @list_of_filenames.each{|filename| Song.new_by_filename(filename)}
   end
 
