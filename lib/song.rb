e58d354e55e635738d65ab1547752fa62ca33ce7
Chris Nady <nady.christopher@gmail.com>
2017-09-11 19:08:00 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,7 +13,7 @@ class Song
 
   def self.new_by_filename(filename)
     new_filename = filename.split(" - ")
-    song = Song.new(new_filename[1])
+    song = self.new(new_filename[1])
     song.artist_name = new_filename[0]
   end
 
