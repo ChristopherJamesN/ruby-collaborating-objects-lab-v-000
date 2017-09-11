c3f413575adaa69a8b45dd71b76fdb4a8efb109e
Chris Nady <nady.christopher@gmail.com>
2017-09-11 17:26:44 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,55 @@
+class Artist
+
+  attr_accessor :name
+  @@song_count=0
+  @@all = []
+
+  def initialize(name="Placeholder")
+    @songs = []
+    @name = name
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+
+  def self.song_count
+    @@song_count
+  end
+
+  def songs
+    @songs
+  end
+
+  def save
+    @@all.push(self)
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_or_create_by_name(name)
+    if @@all.include?(name) == true
+      self.name
+    else
+      artist = Artist.new(name)
+    end
+  end
+
+  def print_songs
+    @songs.each do |song|
+      puts "#{song.name}"
+    end
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,26 @@
+class Song
+
+  attr_accessor :name, :artist
+
+  def initialize(name="Placeholder")
+    @name = name
+  end
+
+  def artist_name
+    if self.artist == nil
+      return nil
+    else
+      self.artist.name
+    end
+  end
+
+  def self.new_by_filename(filename)
+    song = self.new
+    new_filename = filename.split(" - ")
+    song.name = new_filename[1]
+    new_artist = Artist.new(new_filename[0])
+    song.artist = new_artist
+    song
+  end
+
+end
