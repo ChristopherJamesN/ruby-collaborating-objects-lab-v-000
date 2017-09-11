c6aefacdf87580478808b414f351502b5b46a2c7
Chris Nady <nady.christopher@gmail.com>
2017-09-11 19:00:09 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,7 +8,7 @@ class Song
 
   def artist_name=(filename)
     new_artist = Artist.find_or_create_by_name(filename)
-    new_artist.add_song(self)
+    new_artist.add_song_by_name(@name)
   end
 
   def self.new_by_filename(filename)
